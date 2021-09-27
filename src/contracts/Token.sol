617d81a6dc05bdc517dabaab5fc956ea318c1e49
Vitaly <tourervit@gmail.com>
2021-09-27 19:34:07 +0300

    Add token transfer test

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -9,7 +9,16 @@ contract Token {
 	uint256 public decimals = 18;
 	uint256 public totalSupply;
 
+	mapping(address => uint256) public balanceOf;
+
 	constructor() public {
-		totalSupply = 100 * (10 * decimals);
+		totalSupply = 100 * (10**decimals);
+		balanceOf[msg.sender] = totalSupply;
+	}
+
+	function transfer(address _to, uint256 _value) public returns (bool success) {
+		balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+		balanceOf[_to] = balanceOf[_to] + _value;
+		return true;
 	}
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,12 +1,17 @@
+const Web3 = require('web3');
 const Token = artifacts.require('./Token');
 
 require('chai').use(require('chai-as-promised')).should();
 
 contract('Token', accounts => {
+	const web3 = new Web3();
+
+	const deployer = accounts[0];
+	const receiver = accounts[1];
 	const name = 'EMG';
 	const symbol = 'EMG';
 	const decimals = 18 + '';
-	const totalSupply = (100 * (10 * decimals)).toString();
+	const totalSupply = web3.utils.toWei('100', 'ether');
 
 	let token;
 	beforeEach(async () => {
@@ -29,9 +34,30 @@ contract('Token', accounts => {
 			result.toString().should.equal(decimals);
 		});
 
-		it('tracks the decimals', async () => {
+		it('tracks the totalSupply', async () => {
 			const result = await token.totalSupply();
 			result.toString().should.equal(totalSupply);
+		});
+
+		it('assigns all the tokens to deployer', async () => {
+			const result = await token.balanceOf(deployer);
+			result.toString().should.equal(totalSupply);
+		});
+	});
+
+	describe('sending tokens', () => {
+		it('transfers token balances', async () => {
+			const deployerBalanceBeforeTransfer = await token.balanceOf(deployer);
+			console.log('deployerBalance: ', deployerBalanceBeforeTransfer.toString());
+			const receiverBalanceBeforeTransfer = await token.balanceOf(receiver);
+			console.log('receiverBalance: ', receiverBalanceBeforeTransfer.toString());
+
+			await token.transfer(receiver, web3.utils.toWei('1', 'ether'), { from: deployer });
+
+			const deployerBalanceAfterTransfer = await token.balanceOf(deployer);
+			console.log('deployerBalance: ', deployerBalanceAfterTransfer.toString());
+			const receiverBalanceAfterTransfer = await token.balanceOf(receiver);
+			console.log('receiverBalance: ', receiverBalanceAfterTransfer.toString());
 		});
 	});
 });
