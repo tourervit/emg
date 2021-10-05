301ef648179d2e021652577671f2be27f98c408f
Vitaly <tourervit@gmail.com>
2021-10-05 21:26:45 +0300

    Add Order operations

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -11,9 +11,40 @@ contract Exchange {
 	address constant ETH_ADDRESS = address(0);
 	// token address => user address => actual number of tokens
 	mapping(address => mapping(address => uint)) public tokens; 
+	mapping(uint => _Order) public orders;
+	mapping(uint => bool) public canceledOrders;
+	uint public orderCount;
 	
 	event Deposit(address token, address user, uint amount, uint balance);
 	event Withdraw(address token, address user, uint amount, uint balance);
+	event Order(
+		uint id,
+		address user,
+		address tokenBuy,
+		uint amountBuy,
+		address tokenSell,
+		uint amountSell,
+		uint timestamp
+	);
+	event CancelOrder(
+		uint id,
+		address user,
+		address tokenBuy,
+		uint amountBuy,
+		address tokenSell,
+		uint amountSell,
+		uint timestamp
+	);
+	
+	struct _Order {
+		uint id;
+		address user;
+		address tokenBuy;
+		uint amountBuy;
+		address tokenSell;
+		uint amountSell;
+		uint timestamp;
+	}
 	
 	constructor(address _feeAccount, uint _feePercent) public {
 		feeAccount = _feeAccount;
@@ -56,4 +87,19 @@ contract Exchange {
 	function balanceOf(address _token, address _user) public view returns(uint) {
 		return tokens[_token][_user];
 	}
+	
+	function makeOrder(address _tokenBuy, uint _amountBuy, address _tokenSell, uint _amountSell) public {
+		orderCount = orderCount + 1;
+		orders[orderCount] = _Order(orderCount, msg.sender, _tokenBuy, _amountBuy, _tokenSell, _amountSell, block.timestamp);
+		emit Order(orderCount, msg.sender, _tokenBuy, _amountBuy, _tokenSell, _amountSell, block.timestamp);
+	}
+	
+	function cancelOrder(uint _id) public {
+		_Order memory _order = orders[_id];
+		require(address(_order.user) == msg.sender);
+		require(_order.id == _id);
+		canceledOrders[_id] = true;
+		emit CancelOrder(orderCount, msg.sender, _order.tokenBuy, _order.amountBuy, _order.tokenSell, _order.amountSell, block.timestamp);
+	}
+		
 }
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -7,7 +7,7 @@ const ETH_ADDRESS = '0x0000000000000000000000000000000000000000';
 require('chai').use(require('chai-as-promised')).should();
 
 contract('Exchange', accounts => {
-	const [deployer, feeAccount, user1] = accounts;
+	const [deployer, feeAccount, user1, user2] = accounts;
 	const feePercent = 10;
 	let token;
 	let exchange;
@@ -196,6 +196,107 @@ contract('Exchange', accounts => {
 		it('correctly reads user balance', async () => {
 			const result = await exchange.balanceOf(ETH_ADDRESS, user1);
 			result.toString().should.equal(web3.utils.toWei('1', 'ether'));
+		});
+	});
+
+	describe('order init', async () => {
+		let result;
+		beforeEach(async () => {
+			result = await exchange.makeOrder(
+				token.address,
+				web3.utils.toWei('1', 'ether'),
+				ETH_ADDRESS,
+				web3.utils.toWei('1', 'ether'),
+				{ from: user1 },
+			);
+		});
+
+		it('tracks the new order', async () => {
+			const orderCount = await exchange.orderCount();
+			orderCount.toString().should.eq('1');
+			const order = await exchange.orders('1');
+			order.id.toString().should.eq('1', 'id is correct');
+			order.tokenBuy.should.eq(token.address, 'tokenBuy is correct');
+			order.amountBuy.toString().should.eq(web3.utils.toWei('1', 'ether'), 'amountBuy is correct');
+			order.tokenSell.should.eq(ETH_ADDRESS, 'tokenSell is correct');
+			order.amountSell
+				.toString()
+				.should.eq(web3.utils.toWei('1', 'ether'), 'amountSell is correct');
+			order.timestamp.toString().length.should.be.at.least(1, 'timestamp is correct');
+		});
+
+		it('emits Order event', () => {
+			const log = result.logs[0];
+			log.event.should.eq('Order');
+			const event = log.args;
+			event.id.toString().should.eq('1', 'id is correct');
+			event.user.should.eq(user1, 'user address is corrent');
+			event.tokenBuy.should.eq(token.address, 'tokenBuy is correct');
+			event.amountBuy.toString().should.eq(web3.utils.toWei('1', 'ether'), 'amountBuy is correct');
+			event.tokenSell.should.eq(ETH_ADDRESS, 'tokenSell is correct');
+			event.amountSell
+				.toString()
+				.should.eq(web3.utils.toWei('1', 'ether'), 'amountSell is correct');
+			event.timestamp.toString().length.should.be.at.least(1, 'timestamp is correct');
+		});
+	});
+
+	describe('orders', async () => {
+		beforeEach(async () => {
+			await exchange.depositEther({ from: user1, value: web3.utils.toWei('1', 'ether') });
+			await exchange.makeOrder(
+				token.address,
+				web3.utils.toWei('1', 'ether'),
+				ETH_ADDRESS,
+				web3.utils.toWei('1', 'ether'),
+				{ from: user1 },
+			);
+		});
+
+		describe('cancel order', async () => {
+			let result;
+
+			describe('success', async () => {
+				beforeEach(async () => {
+					result = await exchange.cancelOrder('1', { from: user1 });
+				});
+
+				it('updates canceledOrders mapping', async () => {
+					const canceledOrder = await exchange.canceledOrders(1);
+					canceledOrder.should.eq(true);
+				});
+
+				it('emits CancelOrder event', () => {
+					const log = result.logs[0];
+					log.event.should.eq('CancelOrder');
+					const event = log.args;
+					event.id.toString().should.eq('1', 'id is correct');
+					event.user.should.eq(user1, 'user address is corrent');
+					event.tokenBuy.should.eq(token.address, 'tokenBuy is correct');
+					event.amountBuy
+						.toString()
+						.should.eq(web3.utils.toWei('1', 'ether'), 'amountBuy is correct');
+					event.tokenSell.should.eq(ETH_ADDRESS, 'tokenSell is correct');
+					event.amountSell
+						.toString()
+						.should.eq(web3.utils.toWei('1', 'ether'), 'amountSell is correct');
+					event.timestamp.toString().length.should.be.at.least(1, 'timestamp is correct');
+				});
+			});
+
+			describe('failure', async () => {
+				it('rejects invalid order id', async () => {
+					await exchange
+						.cancelOrder(666, { from: user1 })
+						.should.be.rejectedWith('VM Exception while processing transaction: revert');
+				});
+
+				it('rejects attemps to cancel foreign order', async () => {
+					await exchange
+						.cancelOrder(1, { from: user2 })
+						.should.be.rejectedWith('VM Exception while processing transaction: revert');
+				});
+			});
 		});
 	});
 });
