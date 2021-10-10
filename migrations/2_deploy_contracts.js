f6eab50a87ffe08214cd4f0ed02694a8f8f3eafa
Vitaly <tourervit@gmail.com>
2021-10-10 21:31:45 +0300

    Add deploy Exchange migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 const Token = artifacts.require('Token');
+const Exchange = artifacts.require('Exchange');
 
-module.exports = function (deployer) {
-	deployer.deploy(Token);
+module.exports = async function (deployer) {
+	const accounts = await web3.eth.getAccounts();
+	await deployer.deploy(Token);
+	await deployer.deploy(Exchange, accounts[0], 10);
 };
